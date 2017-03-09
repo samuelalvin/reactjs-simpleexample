292ceacba4b95dc2015e510eacc1ddd82eff69a8
samuelalvin <samuel.alvin1994@gmail.com>
2017-03-09 17:35:33 +0700

    Implements react and bundle it with webpack

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+/// <reference path="../../typings/modules/react/index.d.ts" />
+/// <reference path="../../typings/modules/react-dom/index.d.ts" />
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <h1>Hello World!</h1>,
+    document.getElementById("app")
+);
