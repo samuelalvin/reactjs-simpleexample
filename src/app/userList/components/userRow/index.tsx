53a17013e5e415c84b35b9cef0042c34876fdf2c
Samuel, Hutama <Samuel.Hutama@palador.com>
2017-05-09 14:19:41 +0700

    delete crud functions

--- src/app/userList/components/userRow/index.tsx
+++ src/app/userList/components/userRow/index.tsx
@@ -4,96 +4,17 @@ import { IUser } from "../../../interface/user";
 
 interface UserRowProps {
     user: IUser;
-
-    onUserDeletion(key: number): void;
-    onUserEdit(updatedUser: IUser): void;
 }
 
-interface UserRowState {
-    updatedUser?: IUser;
-    editMode?: boolean;
-}
+interface UserRowState { }
 
 export class UserRow extends React.Component<UserRowProps, UserRowState> {
-    constructor(props) {
-        super(props);
-        
-        this.handleUserEdit = this.handleUserEdit.bind(this);
-        this.enableEditMode = this.enableEditMode.bind(this);
-        this.disableEditMode = this.disableEditMode.bind(this);
-        this.onUserInformationChanges = this.onUserInformationChanges.bind(this);
-        this.state = {
-            updatedUser: Object.assign({} as IUser, this.props.user),
-            editMode: false
-        };
-    }
-
-    private handleUserDeletion(key: number): void {
-        this.props.onUserDeletion(key);
-    }
-
-    private handleUserEdit(): void {
-        let isNameValid = this.state.updatedUser.name.length > 0;
-        let isAgeValid = this.state.updatedUser.age > 0;
-        if (isNameValid === false || isAgeValid === false) {
-            alert("Updated User information is invalid");
-            return;
-        }
-
-        this.props.onUserEdit(this.state.updatedUser);
-        this.disableEditMode();
-    }
-
-    private onUserInformationChanges(event: React.FormEvent): void {
-        const target = event.target as React.HTMLAttributes;
-        let updatedUser = this.state.updatedUser;
-        updatedUser[target.name] = target.value;
-        this.setState({
-            updatedUser: updatedUser
-        });
-    }
-
-    private enableEditMode(): void {
-        this.setState({
-            updatedUser: Object.assign({} as IUser, this.props.user),
-            editMode: true
-        });
-    }
-
-    private disableEditMode(): void {
-        this.setState({
-            editMode: false
-        });
-    }
-
     render(): JSX.Element {
-        if (this.state.editMode) {
-            return (
-                <tr>
-                    <td>{this.props.user.id}</td>
-                    <td>
-                        <input name="name" type="text" value={this.state.updatedUser.name} onChange={this.onUserInformationChanges} />
-                    </td>
-                    <td>
-                        <input name="age" type="number" value={this.state.updatedUser.age.toString()} onChange={this.onUserInformationChanges} />
-                    </td>
-                    <td>
-                        <button onClick={this.handleUserEdit}>Save</button>
-                        <button onClick={this.disableEditMode}>Cancel</button>
-                    </td>
-                </tr>
-            );
-        };
-
         return (
             <tr>
                 <td>{this.props.user.id}</td>
                 <td>{this.props.user.name}</td>
                 <td>{this.props.user.age}</td>
-                <td>
-                    <button onClick={this.enableEditMode}>Edit</button>
-                    <button onClick={(e) => this.handleUserDeletion(this.props.user.id)}>Delete</button>
-                </td>
             </tr>
         );
     }
--- src/app/userList/components/userTable/index.tsx
+++ src/app/userList/components/userTable/index.tsx
@@ -7,8 +7,7 @@ import { UserRow } from "../userRow";
 interface UserTableProps { }
 
 interface UserTableState {
-    users?: IUser[];
-    newUser?: IUser;
+    users: IUser[];
 }
 
 export class UserTable extends React.Component<UserTableProps, UserTableState> {
@@ -16,73 +15,14 @@ export class UserTable extends React.Component<UserTableProps, UserTableState> {
 
     constructor(props) {
         super(props);
-
-        this.deleteUser = this.deleteUser.bind(this);
-        this.editUser = this.editUser.bind(this);
-        this.addUser = this.addUser.bind(this);
-        this.onNewUserInformationChanges = this.onNewUserInformationChanges.bind(this);
         this.state = {
-            users: mockUsers,
-            newUser: {
-                id: 0,
-                name: "",
-                age: 0
-            } as IUser
+            users: mockUsers
         };
     };
 
-    private deleteUser(key: number): void {
-        let userIndex = this.state.users.findIndex(user => user.id === key);
-        let updatedUsers = this.state.users;
-        updatedUsers.splice(userIndex, 1);
-        this.setState({
-            users: updatedUsers
-        });
-    }
-
-    private editUser(updatedUser: IUser): void {
-        let updatedUsers = this.state.users;
-        let updatedUserIndex = updatedUsers.findIndex(user => user.id == updatedUser.id);
-        updatedUsers.splice(updatedUserIndex, 1, updatedUser);
-        this.setState({
-            users: updatedUsers
-        });
-    }
-
-    private addUser(): void {
-        let isIdValid = this.state.newUser.id > 0 && this.state.users.find(user => user.id == this.state.newUser.id) === undefined;
-        let isNameValid = this.state.newUser.name.length > 0;
-        let isAgeValid = this.state.newUser.age > 0;
-        if (isIdValid === false || isNameValid === false || isAgeValid === false) {
-            alert("New User information is invalid");
-            return;
-        }
-
-        let newUser = this.state.newUser;
-        let updatedUsers = this.state.users;
-        updatedUsers.push(newUser);
-        this.setState({
-            users: updatedUsers,
-            newUser: {
-                id: 0,
-                name: "",
-                age: 0
-            } as IUser
-        });
-    }
-
-    private onNewUserInformationChanges(event: React.FormEvent): void {
-        const target = event.target as React.HTMLAttributes;
-        let newUser = this.state.newUser;
-        newUser[target.name] = target.value;
-        this.setState({
-            newUser: newUser 
-        });
-    }
-
     private getUserRows(): void {
         this.userRows = this.state.users.map(mockUser => {
-            return (<UserRow user={mockUser} onUserDeletion={this.deleteUser} onUserEdit={this.editUser} key={mockUser.id} />);
+            return (<UserRow user={mockUser} key={mockUser.id} />);
         })
     }
 
@@ -96,25 +36,10 @@ export class UserTable extends React.Component<UserTableProps, UserTableState> {
                         <th>Id</th>
                         <th>Name</th>
                         <th>Age</th>
-                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {this.userRows}
-                    <tr>
-                        <td>
-                            <input name="id" type="number" value={this.state.newUser.id.toString()} onChange={this.onNewUserInformationChanges} />
-                        </td>
-                        <td>
-                            <input name="name" type="text" value={this.state.newUser.name} onChange={this.onNewUserInformationChanges} />
-                        </td>
-                        <td>
-                            <input name="age" type="number" value={this.state.newUser.age.toString()} onChange={this.onNewUserInformationChanges} />
-                        </td>
-                        <td>
-                            <button onClick={this.addUser}>Add a User</button>
-                        </td>
-                    </tr>
                 </tbody>
             </table>
         );
